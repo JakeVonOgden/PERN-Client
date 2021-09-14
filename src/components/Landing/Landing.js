4f8b2d2c49993ff0acb5bcf1e8417a3fe32eb155
Jacob Von Ogden <jvon1197@gmail.com>
2021-09-14 19:25:01 -0400

    Initial struture setup

---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Container} from 'reactstrap'
+
+const Landing = () => {
+    return (
+        <Container>
+        
+        </Container>
+    )
+}
+
+export default Landing;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Container} from 'reactstrap'
+
+const Navbar = () => {
+    return (
+        <Container>
+           
+        </Container>
+    )
+}
+
+export default Navbar;
