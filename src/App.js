342615d853fb69aa8c9a5648dd125aaa4a05998c
Jacob Von Ogden <jvon1197@gmail.com>
2021-09-14 19:34:49 -0400

    Mounted navbar and landing components into app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
-
+import Navbar from './components/Navbar/Navbar';
+import Landing from './components/Landing/Landing';
 
 function App() {
+  
   return (
+    
     <div className="App">
+     <Navbar />
+     <Landing />
      
     </div>
+  
   );
 }
 
