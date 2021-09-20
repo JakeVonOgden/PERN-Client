ba48a1381186c65cf5897dd764ebe1096397d60a
Cheryl Stone <cheryl.stone34@gmail.com>
2021-09-20 19:20:47 -0400

    merging develop code into cheryl branch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import React from 'react';
 import './App.css';
 import React, {useState, useEffect} from 'react';
 import Navbar from './components/Navbar/Navbar';
-import Landing from './components/Landing/Landing';
+
 import Auth from './components/Auth/Auth';
 import Placeholder from './components/Merchandise/Placeholder';
-<<<<<<< HEAD
-=======
 import Footer from './components/sites/Footer';
 import {BrowserRouter as Router} from 'react-router-dom';
 
 
->>>>>>> 64859ae40b7fdc4ebdfe77e120a3c0e2a11a7fcd
 
 function App() {
   
@@ -44,17 +40,12 @@ const protectedViews =() => {
     
     <div className="App">
      <Navbar />
-<<<<<<< HEAD
-     <Landing />
-     {protectedViews()}
-=======
 
-     <Landing />
+    
      {protectedViews()}
 
      <Router />
 
->>>>>>> 64859ae40b7fdc4ebdfe77e120a3c0e2a11a7fcd
      
 
 
