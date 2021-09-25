20cb5d9a6b4af59543842058a4caa73ca7874def
Jacob Von Ogden <jvon1197@gmail.com>
2021-09-25 11:20:20 -0400

    fixing heroku maybe again?!?

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,7 +4,7 @@ switch(window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'https://juniper-server.herokuapp.com/';
         break;
-    case 'jsc-juniper-client.herokuapp.com/':
+    case 'juniper-server.herokuapp.com/':
         APIURL = 'https://juniper-server.herokuapp.com/';
 }
 
