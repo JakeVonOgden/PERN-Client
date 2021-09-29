f44a41dcd6ad5706fb1195c50b188800de0bfba7
Jacob Von Ogden <jvon1197@gmail.com>
2021-09-28 21:09:35 -0400

    added redirect

--- src/components/Auth/Account.js
+++ src/components/Auth/Account.js
@@ -1,6 +1,13 @@
 import MerchandiseIndex from "../Merchandise/MerchandiseIndex";
+import { Redirect } from "react-router";
 
 const Account = (props) => {
+    
+    if (props.sessionToken === ''){
+        alert("we dont know you")
+        return <Redirect to ="/login" />
+    }
+    
     return (
         <>
             <MerchandiseIndex sessionToken={props.sessionToken} />
